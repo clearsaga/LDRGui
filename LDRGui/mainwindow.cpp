63880b5c6cbfd0f8fe3145e7e77e76af933acf8a
clearsaga <clearsaga@gmail.com>
2012-07-02 00:40:39 +0800

    practice

--- LDRGui/mainwindow.cpp
+++ LDRGui/mainwindow.cpp
@@ -1,14 +1,83 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QTimer>
+
+QStatusBar *gStatus;
+QLabel *gBulb;
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    gStatus = ui->statusBar;
+    gBulb = ui->lbl_bulb;
+    MainWindow::init();
+    QTimer::singleShot(100,this,SLOT(loop()));
+
 }
+
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_btn_on_clicked()
+{
+
+}
+
+void MainWindow::loop(void){
+    // input from 0x379
+    ldr = parIn();int modest = 3;
+
+    if( modest == MODE_ON ){
+        parOut(1);
+    } else if( modest == MODE_OFF ){
+        parOut(0);
+    } else if( modest == MODE_DARK ){
+        if( ldr == 0 ){
+            parOut(1);
+        } else {
+            parOut(0);
+        }
+    } else if( modest == MODE_LIGHT ){
+        if( ldr == 1 ){
+            parOut(1);
+        } else {
+            parOut(0);
+        }
+    } else {
+        QString err = "Mode:Error";
+        gStatus->showMessage(err);
+        return;
+    }
+
+    led = parCheck();
+    if( led == 0 ){
+        gBulb->setPixmap(bulbOff);
+    } else {
+
+    }
+
+    //gStatus->showMessage("Mode:" + (char)mode + " Sensor:" + (char)ldr + "Output:" + (char)led);
+}
+
+void MainWindow::init(){
+    bulbOn.load("BulbOn.png");
+    bulbOff.load("BulbOff.png");
+}
+
+short MainWindow::parIn(){
+    return 1;
+}
+
+void MainWindow::parOut( short datar ){
+   ;
+}
+
+short MainWindow::parCheck(){
+    return 0;
 }
--- LDRGui/mainwindow.h
+++ LDRGui/mainwindow.h
@@ -1,7 +1,12 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
+#define MODE_ON 0
+#define MODE_OFF 1
+#define MODE_DARK 2
+#define MODE_LIGHT 3
 
 #include <QMainWindow>
+#include <QPixmap>
 
 namespace Ui {
 class MainWindow;
@@ -14,9 +19,29 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-    
+    short ldr;
+    short led;
+
+
+private slots:
+
+    void on_btn_on_clicked();
+
+    void loop();
+
 private:
     Ui::MainWindow *ui;
+
+    QPixmap bulbOn;
+    QPixmap bulbOff;
+
+    void init();
+    short parIn();
+    void parOut( short data );
+    short parCheck();
+
 };
+
+
 
 #endif // MAINWINDOW_H
